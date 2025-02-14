1ebf3d88a54185be9513ce2a50420a6d224bbfec
Maddy Buitenhuis <77648909+mlbuiten515@users.noreply.github.com>
2025-02-14 13:33:20 +0000

    created data generation python script

---
+++ data_generation.py
@@ -0,0 +1,3 @@
+# This file will be used to generate artifical data for seat demand on a flight by flight basis
+# The minimum ticket price will be determined by location and season demand
+# available sseat number will be determined by standard aircraft size for the route, potential to add as a vraiable in the future
